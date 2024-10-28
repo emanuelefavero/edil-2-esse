d696b709e874242fbecdbaa271bb3fe48078c2e1
emanuelefavero <emafaux89@gmail.com>
2024-10-28 16:16:58 +0100

    fix next.js Image warnings

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -11,9 +11,10 @@ export default function Component() {
           window.scrollTo(0, 0)
           window.location.reload()
         }}
-        className='rounded-sm outline-none focus:shadow-[0_0_0_1px] focus:shadow-stone-500/80 dark:focus:shadow-stone-500/80'
+        className='w-[68px] rounded-sm outline-none focus:shadow-[0_0_0_1px] focus:shadow-stone-500/80 dark:focus:shadow-stone-500/80'
+        aria-label='Go to home'
       >
-        <Image src='/logo.png' alt='Logo' width={68} height={68} />
+        <Image src='/logo.png' alt='Logo' width={250} height={145} priority />
       </button>
     </>
   )
