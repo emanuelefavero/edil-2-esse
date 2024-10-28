eb7bb31f291ba342e98357fc23464f40ac92afe8
emanuelefavero <emafaux89@gmail.com>
2024-10-28 11:58:42 +0100

    add radix ui primitives, add hamburger menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+import DropdownMenu from '@/components/DropdownMenu'
+
 export default function Home() {
   return (
     <>
+      <header className='w-full flex justify-between items-center px-2 py-2'>
+        <div>Logo</div>
+        <DropdownMenu />
+      </header>
       <h1>EDIL 2 ESSE</h1>
     </>
   )
---
+++ components/DropdownMenu.tsx
@@ -0,0 +1,42 @@
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+import { HamburgerMenuIcon } from '@radix-ui/react-icons'
+
+export default function DropdownMenuDemo() {
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger asChild>
+        <button
+          className='inline-flex items-center justify-center px-[3.5px] rounded-sm text-black dark:text-white outline-none focus:shadow-[0_0_0_1px] focus:shadow-stone-500/80 dark:focus:shadow-stone-500/80 active:scale-95'
+          aria-label='Open dropdown menu'
+        >
+          <HamburgerMenuIcon className='size-[40px] text-stone-700 dark:text-stone-200' />
+        </button>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content
+          className='rounded-md bg-white dark:bg-stone-900 p-[5px] mr-2 shadow-sm shadow-stone-600/20 will-change-[opacity,transform] data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade data-[side=right]:animate-slideLeftAndFade data-[side=top]:animate-slideDownAndFade border-[0.5px] border-stone-500/80'
+          sideOffset={3}
+        >
+          <DropdownMenu.Item className='group relative flex select-none items-center rounded-[3px] px-2 py-2 text-base leading-none text-stone-700 dark:text-stone-200 font-medium outline-none data-[disabled]:pointer-events-none data-[disabled]:text-stone-400 dark:data-[disabled]:text-stone-500 data-[highlighted]:text-amber-600'>
+            Home
+          </DropdownMenu.Item>
+
+          <DropdownMenu.Item className='group relative flex select-none items-center rounded-[3px] px-2 py-2 text-base leading-none text-stone-700 dark:text-stone-200 font-medium outline-none data-[disabled]:pointer-events-none data-[disabled]:text-stone-400 dark:data-[disabled]:text-stone-500 data-[highlighted]:text-amber-600'>
+            Servizi
+          </DropdownMenu.Item>
+
+          <DropdownMenu.Item className='group relative flex select-none items-center rounded-[3px] px-2 py-2 text-base leading-none text-stone-700 dark:text-stone-200 font-medium outline-none data-[disabled]:pointer-events-none data-[disabled]:text-stone-400 dark:data-[disabled]:text-stone-500 data-[highlighted]:text-amber-600'>
+            Chi Siamo
+          </DropdownMenu.Item>
+
+          <DropdownMenu.Item className='group relative flex select-none items-center rounded-[3px] px-2 py-2 text-base leading-none text-stone-700 dark:text-stone-200 font-medium outline-none data-[disabled]:pointer-events-none data-[disabled]:text-stone-400 dark:data-[disabled]:text-stone-500 data-[highlighted]:text-amber-600'>
+            Contatti
+          </DropdownMenu.Item>
+
+          <DropdownMenu.Arrow className='fill-stone-500/50' />
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+  )
+}
