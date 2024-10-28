47ef265e3ff6b676249d531286173b9794acb2c7
emanuelefavero <emafaux89@gmail.com>
2024-10-28 21:13:01 +0100

    add select-none to all header elements

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,18 +4,16 @@ import Image from 'next/image'
 
 export default function Component() {
   return (
-    <>
-      <button
-        type='button'
-        onClick={() => {
-          window.scrollTo(0, 0)
-          window.location.reload()
-        }}
-        className='w-[68px] rounded-sm outline-none focus:shadow-[0_0_0_1px] focus:shadow-stone-500/80 dark:focus:shadow-stone-500/80'
-        aria-label='Go to home'
-      >
-        <Image src='/logo.png' alt='Logo' width={250} height={145} priority />
-      </button>
-    </>
+    <button
+      type='button'
+      onClick={() => {
+        window.scrollTo(0, 0)
+        window.location.reload()
+      }}
+      className='w-[68px] rounded-sm outline-none focus:shadow-[0_0_0_1px] focus:shadow-stone-500/80 dark:focus:shadow-stone-500/80 select-none'
+      aria-label='Go to home'
+    >
+      <Image src='/logo.png' alt='Logo' width={250} height={145} priority />
+    </button>
   )
 }
