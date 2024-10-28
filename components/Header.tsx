b1a0247203fab218fda22448d9d133f820b0f8e3
emanuelefavero <emafaux89@gmail.com>
2024-10-28 20:01:27 +0100

    add backdrop blur in Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Logo from '@/components/Logo'
 
 export default function Home() {
   return (
-    <header className='w-full flex justify-between items-center px-2 py-2 border-b border-stone-500/80'>
+    <header className='fixed w-full flex justify-between items-center px-2 py-2 border-b border-stone-500/80 bg-stone-100/90 dark:bg-[rgba(21,18,16,0.9)] backdrop-blur-md'>
       <Logo />
       <DropdownMenu />
     </header>
