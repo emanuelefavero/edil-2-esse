a9748a127d0d1b9cdf6b95e77a84a800263415bc
emanuelefavero <emafaux89@gmail.com>
2024-10-28 21:25:27 +0100

    add useScrollDirection and hide header on scroll direction

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,18 @@
+'use client'
+
+import { useScrollDirection } from '@/hooks/useScrollDirection'
 import DropdownMenu from '@/components/DropdownMenu'
 import Logo from '@/components/Logo'
 
 export default function Home() {
+  const scrollDirection = useScrollDirection()
+
   return (
-    <header className='fixed w-full flex justify-between items-center px-2 py-2 border-b-[3px] border-yellow-500 dark:border-amber-500/80 rounded-b-md bg-stone-100/90 dark:bg-[rgba(21,18,16,0.9)] backdrop-blur-md shadow-[0_0_0_3px] shadow-amber-600/80 dark:shadow-yellow-700/80'>
+    <header
+      className={`fixed w-full flex justify-between items-center px-2 py-2 border-b-[3px] border-yellow-500 dark:border-amber-500/80 rounded-b-md bg-stone-100/90 dark:bg-[rgba(21,18,16,0.9)] backdrop-blur-md shadow-[0_0_0_3px] shadow-amber-600/80 dark:shadow-yellow-700/80 transition-transform duration-200 ${
+        scrollDirection === 'down' ? '-translate-y-full' : 'translate-y-0'
+      }`}
+    >
       <Logo />
       <DropdownMenu />
     </header>
