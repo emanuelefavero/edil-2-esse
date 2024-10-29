a7d28bb37fcfd673c9b30c6de18e537696e82a3d
emanuelefavero <emafaux89@gmail.com>
2024-10-29 14:54:15 +0100

    add Button component and hero button

---
+++ components/Button.tsx
@@ -0,0 +1,41 @@
+import { ReactNode, Ref } from 'react'
+import clsx from 'clsx'
+
+interface Props {
+  children: ReactNode
+  type?: 'button' | 'submit' | 'reset'
+  onClick?: () => void
+  ariaLabel?: string
+  disabled?: boolean
+  className?: string
+  ref?: Ref<HTMLButtonElement>
+}
+
+export default function Component({
+  children,
+  type = 'button',
+  onClick,
+  ariaLabel,
+  disabled = false,
+  className,
+  ref,
+}: Props) {
+  return (
+    <>
+      <button
+        type={type}
+        onClick={onClick}
+        aria-label={ariaLabel}
+        disabled={disabled}
+        className={clsx(
+          'bg-amber-500 border-[1px] border-amber-400 shadow-[0.5px_0.5px_0.5px_0.8px] shadow-yellow-800/80 font-[600] text-2xl tracking-wide px-5 py-1.5 rounded-lg text-white hover:bg-yellow-600 duration-200 active:transform active:translate-y-0.5 active:scale-[0.99] active:shadow-none transition-all',
+          className,
+          disabled && 'opacity-50 cursor-not-allowed'
+        )}
+        ref={ref}
+      >
+        {children}
+      </button>
+    </>
+  )
+}
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,7 +1,11 @@
+import Button from '@/components/Button'
+
 export default function Component() {
   return (
-    <section className='w-full h-[calc(100vh-104px)] pt-8 flex flex-col justify-center items-center bg-hero-image bg-cover bg-center'>
+    <section className='w-full h-[calc(100vh-104px)] pt-8 flex flex-col justify-center items-center gap-8 bg-hero-image bg-cover bg-center'>
       <h1 className='text-white font-semibold text-2xl'>EDIL 2 ESSE</h1>
+
+      <Button>Servizi</Button>
     </section>
   )
 }
