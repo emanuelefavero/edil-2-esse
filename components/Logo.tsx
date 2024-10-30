8b290a582a56d00cb1ab3368dcfb4d4bea2a1b1d
emanuelefavero <emafaux89@gmail.com>
2024-10-30 14:16:41 +0100

    add - navigate home when logo is clicked if not home

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,21 +1,31 @@
 'use client'
 
+import { usePathname, useRouter } from 'next/navigation'
 import Image from 'next/image'
 
-// TODO: Check if the current route is the home page, if so, reload the page, if not, navigate to the home page
-
 export default function Component() {
+  const pathname = usePathname()
+  const router = useRouter()
+
+  const handleClick = () => {
+    if (pathname === '/') {
+      window.scrollTo(0, 0)
+      window.location.reload()
+    } else {
+      router.push('/')
+    }
+  }
+
   return (
-    <button
-      type='button'
-      onClick={() => {
-        window.scrollTo(0, 0)
-        window.location.reload()
-      }}
-      className='w-[68px] rounded-sm outline-none focus:shadow-[0_0_0_1px] focus:shadow-stone-500/80 dark:focus:shadow-stone-500/80 select-none'
-      aria-label='Go to home'
-    >
-      <Image src='/logo.png' alt='Logo' width={250} height={145} priority />
-    </button>
+    <>
+      <button
+        type='button'
+        onClick={handleClick}
+        className='w-[68px] rounded-sm outline-none focus:shadow-[0_0_0_1px] focus:shadow-stone-500/80 dark:focus:shadow-stone-500/80 select-none'
+        aria-label='Go to home'
+      >
+        <Image src='/logo.png' alt='Logo' width={250} height={145} priority />
+      </button>
+    </>
   )
 }
