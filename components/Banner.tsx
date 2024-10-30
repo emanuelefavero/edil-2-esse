5fdd3649bbf75cd927bd4754dc2889f2e5f8430c
emanuelefavero <emafaux89@gmail.com>
2024-10-30 19:33:40 +0100

    refactor Banner component

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,43 +1,54 @@
 import ZigZagBorder from '@/components/ZigZagBorder'
-import { bannerItems } from '@/data/bannerItems'
+import { bannerItems, IBannerItem } from '@/data/bannerItems'
 
-export default function Component() {
+interface Props {
+  item: IBannerItem
+}
+
+function BannerItem({ item }: Props) {
+  const responsiveHide = (id: number) =>
+    id === 4
+      ? 'hidden lg:flex'
+      : id === 3
+      ? 'hidden md:flex'
+      : id === 2
+      ? 'hidden sm:flex'
+      : 'flex'
+
+  const sizeBasedOnSymbol = item.symbol === '%' ? 'text-2xl' : 'text-4xl'
+
+  return (
+    <li
+      key={item.id}
+      className={`flex justify-center items-center ${responsiveHide(item.id)} ${
+        item.id > 1 && 'ml-16 lg:ml-16'
+      }`}
+    >
+      <div className='hidden 5xs:block text-5xl font-bold text-shadow-yellow select-none'>
+        {item.value}
+        <span className={`relative -top-2 ml-0.5 ${sizeBasedOnSymbol}`}>
+          {item.symbol}
+        </span>
+      </div>
+      <div className='text-sm text-shadow-yellow ml-4 select-none'>
+        <span className={`${item.id === 1 ? 'inline 5xs:hidden' : 'hidden'}`}>
+          35+{' '}
+        </span>
+        {item.label}
+      </div>
+    </li>
+  )
+}
+
+export default function Banner() {
   return (
     <>
       <ZigZagBorder />
+
       <section className='min-h-[130px] bg-yellow-600 text-white border-b-[2.5px] border-yellow-700/60 dark:border-yellow-700/80 px-8 2xl:px-16 py-8 flex justify-center items-center font-semibold'>
         <ul className='w-full max-w-screen-2xl flex items-center justify-center'>
           {bannerItems.map((item) => (
-            <li
-              key={item.id}
-              className={` 
-              ${item.id > 1 && 'ml-16 lg:ml-16'}
-              ${item.id === 4 ? 'hidden lg:flex' : 'flex'}
-              ${item.id === 3 ? 'hidden md:flex' : 'flex'}
-              ${item.id === 2 ? 'hidden sm:flex' : 'flex'} 
-              justify-center items-center`}
-            >
-              <div className='hidden 5xs:block text-5xl font-bold text-shadow-yellow select-none'>
-                {item.value}
-                <span
-                  className={`relative -top-2 ml-0.5 ${
-                    item.symbol === '%' ? 'text-2xl' : 'text-4xl'
-                  }`}
-                >
-                  {item.symbol}
-                </span>
-              </div>
-              <div className='text-sm text-shadow-yellow ml-4 select-none'>
-                <span
-                  className={`${
-                    item.id === 1 ? 'inline 5xs:hidden' : 'hidden'
-                  }`}
-                >
-                  35+{' '}
-                </span>
-                {item.label}
-              </div>
-            </li>
+            <BannerItem key={item.id} item={item} />
           ))}
         </ul>
       </section>
--- data/bannerItems.ts
+++ data/bannerItems.ts
@@ -1,4 +1,11 @@
-export const bannerItems = [
+export interface IBannerItem {
+  id: number
+  value: string
+  symbol: string
+  label: string
+}
+
+export const bannerItems: IBannerItem[] = [
   { id: 1, value: '35', symbol: '+', label: 'Anni di esperienza' },
   { id: 2, value: '300', symbol: '+', label: 'Clienti soddisfatti' },
   { id: 3, value: '1000', symbol: '+', label: 'Progetti completati' },
