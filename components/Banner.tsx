566731394f56e5c818828252ab3cf5e468316b8d
emanuelefavero <emafaux89@gmail.com>
2024-10-30 17:30:03 +0100

    add banner items to Banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,11 +1,28 @@
 import ZigZagBorder from '@/components/ZigZagBorder'
+import { bannerItems } from '@/data/bannerItems'
 
 export default function Component() {
   return (
     <>
       <ZigZagBorder />
-      <section className='bg-yellow-600 text-white h-[150px] flex justify-center items-center text-5xl font-semibold'>
-        Banner
+      <section className='bg-yellow-600 text-white h-[130px] flex justify-center items-center gap-16 font-semibold'>
+        {bannerItems.map((item) => (
+          <div key={item.id} className='flex justify-center items-center gap-4'>
+            <div className='text-5xl font-bold text-shadow-yellow'>
+              {item.value}
+              <span
+                className={`relative -top-2 ml-0.5 ${
+                  item.symbol === '%' ? 'text-2xl' : 'text-4xl'
+                }`}
+              >
+                {item.symbol}
+              </span>
+            </div>
+            <div className='text-sm max-w-20 text-shadow-yellow'>
+              {item.label}
+            </div>
+          </div>
+        ))}
       </section>
     </>
   )
--- data/bannerItems.ts
+++ data/bannerItems.ts
@@ -1,6 +1,6 @@
 export const bannerItems = [
-  { id: '1', value: '35+', label: 'Anni di esperienza' },
-  { id: '2', value: '300+', label: 'Clienti soddisfatti' },
-  { id: '3', value: '1000+', label: 'Progetti completati' },
-  { id: '6', value: '100%', label: 'Soddisfazione garantita' },
+  { id: '1', value: '35', symbol: '+', label: 'Anni di esperienza' },
+  { id: '2', value: '300', symbol: '+', label: 'Clienti soddisfatti' },
+  { id: '3', value: '1000', symbol: '+', label: 'Progetti completati' },
+  { id: '4', value: '100', symbol: '%', label: 'Soddisfazione garantita' },
 ]
