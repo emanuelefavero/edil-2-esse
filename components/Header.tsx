4eae36da9230072badad36002be50d126d770306
emanuelefavero <emafaux89@gmail.com>
2024-10-30 15:01:46 +0100

    fix zizag and header z index

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 
   return (
     <header
-      className={`fixed w-full flex justify-center items-center px-2 py-2 border-b-[3px] border-yellow-500 dark:border-amber-500/80 rounded-b-md bg-stone-100/90 dark:bg-[rgba(21,18,16,0.9)] backdrop-blur-md shadow-[0_0_0_3px] shadow-amber-600/80 dark:shadow-yellow-700/80 transition-transform duration-200 ${
+      className={`fixed z-[999] w-full flex justify-center items-center px-2 py-2 border-b-[3px] border-yellow-500 dark:border-amber-500/80 rounded-b-md bg-stone-100/90 dark:bg-[rgba(21,18,16,0.9)] backdrop-blur-md shadow-[0_0_0_3px] shadow-amber-600/80 dark:shadow-yellow-700/80 transition-transform duration-200 ${
         scrollDirection === 'down' ? '-translate-y-full' : 'translate-y-0'
       }`}
     >
