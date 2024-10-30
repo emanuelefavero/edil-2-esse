d1ff275e2175ebec6fa29ee3bb7990af2486d73b
emanuelefavero <emafaux89@gmail.com>
2024-10-30 18:53:01 +0100

    add - hide banner items programmatically

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -5,24 +5,34 @@ export default function Component() {
   return (
     <>
       <ZigZagBorder />
-      <section className='bg-yellow-600 text-white h-[130px] flex justify-center items-center gap-16 font-semibold'>
-        {bannerItems.map((item) => (
-          <div key={item.id} className='flex justify-center items-center gap-4'>
-            <div className='text-5xl font-bold text-shadow-yellow'>
-              {item.value}
-              <span
-                className={`relative -top-2 ml-0.5 ${
-                  item.symbol === '%' ? 'text-2xl' : 'text-4xl'
-                }`}
-              >
-                {item.symbol}
-              </span>
-            </div>
-            <div className='text-sm max-w-20 text-shadow-yellow'>
-              {item.label}
-            </div>
-          </div>
-        ))}
+      <section className='bg-yellow-600 text-white min-h-[130px] px-8 2xl:px-16 py-8 flex justify-center items-center font-semibold'>
+        <ul className='w-full max-w-screen-2xl flex items-center justify-center'>
+          {bannerItems.map((item) => (
+            <li
+              key={item.id}
+              className={` 
+              ${item.id > 1 && 'ml-16 lg:ml-16'}
+              ${item.id === 4 ? 'hidden lg:flex' : 'flex'}
+              ${item.id === 3 ? 'hidden md:flex' : 'flex'}
+              ${item.id === 2 ? 'hidden sm:flex' : 'flex'} 
+              justify-center items-center`}
+            >
+              <div className='text-5xl font-bold text-shadow-yellow'>
+                {item.value}
+                <span
+                  className={`relative -top-2 ml-0.5 ${
+                    item.symbol === '%' ? 'text-2xl' : 'text-4xl'
+                  }`}
+                >
+                  {item.symbol}
+                </span>
+              </div>
+              <div className='text-sm text-shadow-yellow ml-4'>
+                {item.label}
+              </div>
+            </li>
+          ))}
+        </ul>
       </section>
     </>
   )
--- data/bannerItems.ts
+++ data/bannerItems.ts
@@ -1,6 +1,6 @@
 export const bannerItems = [
-  { id: '1', value: '35', symbol: '+', label: 'Anni di esperienza' },
-  { id: '2', value: '300', symbol: '+', label: 'Clienti soddisfatti' },
-  { id: '3', value: '1000', symbol: '+', label: 'Progetti completati' },
-  { id: '4', value: '100', symbol: '%', label: 'Soddisfazione garantita' },
+  { id: 1, value: '35', symbol: '+', label: 'Anni di esperienza' },
+  { id: 2, value: '300', symbol: '+', label: 'Clienti soddisfatti' },
+  { id: 3, value: '1000', symbol: '+', label: 'Progetti completati' },
+  { id: 4, value: '100', symbol: '%', label: 'Soddisfazione garantita' },
 ]
