23869b3b595050a6fd56b36ea79564adda8ff9e3
emanuelefavero <emafaux89@gmail.com>
2024-10-31 19:54:37 +0100

    fix services card dark mode

--- components/ServicesSection.tsx
+++ components/ServicesSection.tsx
@@ -40,11 +40,10 @@ function ServicesList() {
 
 // * Card
 // TODO add shadow to card (e.g. shadow-[1px_1px_3px_1px] shadow-yellow-700/20)
-// TODO fix colors on dark theme
 function ServiceCard({ item }: { item: IServicesItem }) {
   return (
-    <li className='w-fit max-w-[378px] flex flex-col items-center justify-center bg-stone-50 border-2 border-yellow-700/35 rounded-3xl'>
-      <div className='bg-amber-50 border-b border-yellow-700/35 w-full flex justify-center items-center px-2 py-8 rounded-t-3xl'>
+    <li className='w-fit max-w-[378px] flex flex-col items-center justify-center bg-stone-50 dark:bg-[#151210] border-2 border-yellow-700/35 dark:border-yellow-600/35 rounded-3xl'>
+      <div className='bg-amber-50 dark:bg-yellow-950/10 border-b border-yellow-700/35 dark:border-yellow-600/35 w-full flex justify-center items-center px-2 py-8 rounded-t-3xl'>
         <item.icon className='w-20' />
       </div>
       <div className='px-5 pt-4 pb-6 rounded-b-3xl'>
@@ -53,9 +52,9 @@ function ServiceCard({ item }: { item: IServicesItem }) {
           {item.list.map((item, index) => (
             <li key={index} className='w-full pl-3 flex items-center'>
               {/* Circle */}
-              <span className='inline-block flex-shrink-0 w-2 h-2 bg-yellow-600 rounded-full mr-3'></span>
+              <span className='inline-block flex-shrink-0 w-2 h-2 bg-yellow-600 dark:bg-yellow-600 rounded-full mr-3'></span>
 
-              <span className='text-stone-600'>{item}</span>
+              <span className='text-stone-600 dark:text-stone-400'>{item}</span>
             </li>
           ))}
         </ul>
