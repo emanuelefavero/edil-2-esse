60c8b0c2e92e14e6b595f94090e78505a34f3ae8
emanuelefavero <emafaux89@gmail.com>
2024-10-31 14:32:46 +0100

    add services section component

---
+++ components/ServicesSection.tsx
@@ -0,0 +1,7 @@
+export default function Component() {
+  return (
+    <section className='w-full max-w-screen-2xl flex items-center justify-center flex-col px-8 py-20'>
+      <h2>Servizi</h2>
+    </section>
+  )
+}
