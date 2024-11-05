898c90293c17065faa5038added376e5606462a2
emanuelefavero <emafaux89@gmail.com>
2024-11-05 22:35:19 +0100

    move email templates to separate file

--- app/actions.ts
+++ app/actions.ts
@@ -1,12 +1,12 @@
 'use server'
 
 import { Resend } from 'resend'
+import { createOwnerEmail, createUserEmail } from '@/utils/emails'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
+const customDomainEmail = process.env.CUSTOM_DOMAIN_EMAIL as string
 
 export async function sendEstimateRequestEmail(formData: FormData) {
-  const customDomainEmail = process.env.CUSTOM_DOMAIN_EMAIL as string
-
   const name = formData.get('name') as string
   const email = formData.get('email') as string
   const phone = formData.get('phone') as string
@@ -17,12 +17,7 @@ export async function sendEstimateRequestEmail(formData: FormData) {
   }
 
   try {
-    const ownerEmailTemplate = createOwnerEmail({
-      name,
-      email,
-      phone,
-      message,
-    })
+    const ownerEmailTemplate = createOwnerEmail(name, email, phone, message)
     const userEmailTemplate = createUserEmail(name, customDomainEmail)
 
     await Promise.all([
@@ -44,41 +39,4 @@ export async function sendEstimateRequestEmail(formData: FormData) {
   } catch (error) {
     console.error('Error sending estimate request email:', error)
   }
-}
-
-interface OwnerEmailData {
-  name: string
-  email: string
-  phone?: string
-  message: string
-}
-
-function createOwnerEmail({
-  name,
-  email,
-  phone,
-  message,
-}: OwnerEmailData): string {
-  return `
-    <h1>Dettagli della richiesta:</h1>
-    <p><b>Nome:</b> ${name}</p>
-    <p><b>Email:</b> ${email}</p>
-    <p><b>Telefono:</b> ${phone || 'Non specificato'}</p>
-    <p><b>Messaggio:</b> ${message}</p>
-    <p><b>Per rispondere:</b></p>
-    <a href="mailto:${email}">${email}</a>
-  `
-}
-
-function createUserEmail(name: string, customDomainEmail: string): string {
-  return `
-    <img src="https://www.edil2esse.it/logo.png" alt="Logo" width="125" height="72">
-    <h1>Ciao <b>${name}</b>, abbiamo ricevuto il tuo messaggio!</h1>
-    <p>Grazie per averci contattato. Ti risponderemo il prima possibile.</p>
-    <p>Nel frattempo, se hai bisogno di contattarci, scrivici pure a questo indirizzo:</p>
-    <a href="mailto:${customDomainEmail}">${customDomainEmail}</a>
-    <p>A presto e grazie ancora per averci scelto!</p>
-    <p>Il team di <b>EDIL 2 ESSE</b></p>
-    <a href="https://www.edil2esse.it">edil2esse.it</a>
-  `
 }
---
+++ utils/emails.ts
@@ -0,0 +1,32 @@
+export function createOwnerEmail(
+  name: string,
+  email: string,
+  message: string,
+  phone?: string
+): string {
+  return `
+    <h1>Dettagli della richiesta:</h1>
+    <p><b>Nome:</b> ${name}</p>
+    <p><b>Email:</b> ${email}</p>
+    <p><b>Telefono:</b> ${phone || 'Non specificato'}</p>
+    <p><b>Messaggio:</b> ${message}</p>
+    <p><b>Per rispondere:</b></p>
+    <a href="mailto:${email}">${email}</a>
+  `
+}
+
+export function createUserEmail(
+  name: string,
+  customDomainEmail: string
+): string {
+  return `
+    <img src="https://www.edil2esse.it/logo.png" alt="Logo" width="125" height="72">
+    <h1>Ciao <b>${name}</b>, abbiamo ricevuto il tuo messaggio!</h1>
+    <p>Grazie per averci contattato. Ti risponderemo il prima possibile.</p>
+    <p>Nel frattempo, se hai bisogno di contattarci, scrivici pure a questo indirizzo:</p>
+    <a href="mailto:${customDomainEmail}">${customDomainEmail}</a>
+    <p>A presto e grazie ancora per averci scelto!</p>
+    <p>Il team di <b>EDIL 2 ESSE</b></p>
+    <a href="https://www.edil2esse.it">edil2esse.it</a>
+  `
+}
