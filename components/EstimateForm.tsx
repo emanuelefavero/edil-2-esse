aeac11dc30062e551820d746e8ebe10208c579bb
emanuelefavero <emafaux89@gmail.com>
2024-11-05 23:42:22 +0100

    add Input component

--- components/EstimateForm.tsx
+++ components/EstimateForm.tsx
@@ -1,5 +1,6 @@
 import { sendEstimateRequestEmail } from '@/app/actions'
 import Button from '@/components/Button'
+import Input from '@/components/Input'
 
 export default function Component() {
   // TODO Add form submission loading state with useFormStatus hook
@@ -10,55 +11,22 @@ export default function Component() {
   return (
     <div className='w-full flex justify-center mt-20'>
       <form action={sendEstimateRequestEmail} className='w-full max-w-[788px]'>
-        {/* Flex container for inputs */}
         <div className='flex flex-wrap -mx-2'>
-          <div className='w-full md:w-1/2 px-2 mb-5'>
-            <label
-              htmlFor='name'
-              className='block text-sm font-medium text-stone-700 dark:text-stone-200 mb-2'
-            >
-              Nome{' '}
-              <span className='text-yellow-600 dark:text-yellow-500'>*</span>
-            </label>
-            <input
-              type='text'
-              name='name'
-              className='w-full px-2.5 py-2 font-medium dark:bg-stone-800 text-stone-700 dark:text-stone-200 border border-stone-500/35 rounded-lg focus:outline-none focus:ring-0 focus:ring-offset-0 focus:border-yellow-600 dark:focus:border-yellow-700 placeholder:text-stone-400 dark:placeholder:text-stone-500'
-              placeholder='Il tuo Nome'
-              required
-            />
-          </div>
-          <div className='w-full md:w-1/2 px-2 mb-5'>
-            <label
-              htmlFor='phone'
-              className='block text-sm font-medium text-stone-700 dark:text-stone-200 mb-2'
-            >
-              Telefono
-            </label>
-            <input
-              type='tel'
-              name='phone'
-              className='w-full px-2.5 py-2 font-medium dark:bg-stone-800 text-stone-700 dark:text-stone-200 border border-stone-500/35 rounded-lg focus:outline-none focus:ring-0 focus:ring-offset-0 focus:border-yellow-600 dark:focus:border-yellow-700 placeholder:text-stone-400 dark:placeholder:text-stone-500'
-              placeholder='Il tuo numero di telefono'
-              pattern='^(\+39\s?)?((3\d{2}\s?\d{6,7})|(0\d{1,4}\s?\d{5,10}))$'
-            />
-          </div>
-          <div className='w-full md:w-1/2 px-2 mb-5'>
-            <label
-              htmlFor='email'
-              className='block text-sm font-medium text-stone-700 dark:text-stone-200 mb-2'
-            >
-              Email{' '}
-              <span className='text-yellow-600 dark:text-yellow-500'>*</span>
-            </label>
-            <input
-              type='email'
-              name='email'
-              className='w-full px-2.5 py-2 font-medium dark:bg-stone-800 text-stone-700 dark:text-stone-200 border border-stone-500/35 rounded-lg focus:outline-none focus:ring-0 focus:ring-offset-0 focus:border-yellow-600 dark:focus:border-yellow-700 placeholder:text-stone-400 dark:placeholder:text-stone-500'
-              placeholder='La tua email'
-              required
-            />
-          </div>
+          <Input name='name' label='Nome' placeholder='Il tuo nome' required />
+          <Input
+            type='tel'
+            name='phone'
+            label='Telefono'
+            placeholder='Il tuo numero di telefono'
+            pattern='^(\+39\s?)?((3\d{2}\s?\d{6,7})|(0\d{1,4}\s?\d{5,10}))$'
+          />
+          <Input
+            type='email'
+            name='email'
+            label='Email'
+            placeholder='La tua email'
+            required
+          />
         </div>
 
         {/* Full-width message textarea */}
---
+++ components/Input.tsx
@@ -0,0 +1,53 @@
+import { Ref } from 'react'
+import clsx from 'clsx'
+
+interface Props {
+  name: string
+  label: string
+  type?: 'text' | 'email' | 'tel'
+  placeholder?: string
+  required?: boolean
+  pattern?: string
+  ref?: Ref<HTMLInputElement>
+  disabled?: boolean
+  className?: string
+}
+
+export default function Component({
+  name,
+  label,
+  type = 'text',
+  placeholder = '',
+  required = false,
+  pattern = '',
+  ref,
+  disabled = false,
+  className,
+}: Props) {
+  return (
+    <div className='w-full md:w-1/2 px-2 mb-5'>
+      <label
+        htmlFor={name}
+        className='block text-sm font-medium text-stone-700 dark:text-stone-200 mb-2'
+      >
+        {label}{' '}
+        {required && (
+          <span className='text-yellow-600 dark:text-yellow-500'>*</span>
+        )}
+      </label>
+      <input
+        type={type}
+        name={name}
+        className={clsx(
+          'w-full px-2.5 py-2 font-medium dark:bg-stone-800 text-stone-700 dark:text-stone-200 border border-stone-500/35 rounded-lg focus:outline-none focus:ring-0 focus:ring-offset-0 focus:border-yellow-600 dark:focus:border-yellow-700 placeholder:text-stone-400 dark:placeholder:text-stone-500',
+          className,
+          disabled && 'opacity-50 cursor-not-allowed'
+        )}
+        ref={ref}
+        placeholder={placeholder}
+        required={required}
+        pattern={pattern}
+      />
+    </div>
+  )
+}
