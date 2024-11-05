f85f3b0c9d36e210c484cbfbf9a4815c593efbc5
emanuelefavero <emafaux89@gmail.com>
2024-11-05 21:54:04 +0100

    add user html email

--- app/actions.ts
+++ app/actions.ts
@@ -12,14 +12,14 @@ export async function sendEstimateRequestEmail(formData: FormData) {
   const phone = formData.get('phone') as string
   const message = formData.get('message') as string
 
-  // Send an email to the contact email address with the estimate request details
+  // Send an email to the owner with the estimate request details
   try {
     await resend.emails.send({
       from: customDomainEmail,
       to: customDomainEmail,
       subject: `Richiesta di preventivo da ${name}`,
 
-      // TODO Replace email text with html or react (see resend documentation)
+      // TODO Replace email text with html
       text: `
         Nome: ${name}
         Email: ${email}
@@ -29,20 +29,38 @@ export async function sendEstimateRequestEmail(formData: FormData) {
     })
 
     // Send an email to the user to confirm that their estimate request was received
+    const userEmail = `<img src="https://www.edil2esse.it/logo.png" alt="Logo" width="125" height="72">
+
+  <h1>
+    Ciao <b>${name}</b>, abbiamo ricevuto il tuo messaggio!
+  </h1>
+
+  <p>Grazie per averci contattato. Ti risponderemo il prima possibile.</p>
+
+  <p>
+    Nel frattempo, se hai bisogno di contattarci, scrivici pure a questo
+    indirizzo:
+  </p>
+
+  <a href="mailto:${customDomainEmail}">
+      ${customDomainEmail}
+  </a>
+
+  <p>A presto e grazie ancora per averci scelto!</p>
+
+  <p>Il team di <b>EDIL 2 ESSE</b></p>
+
+  <a href="https://www.edil2esse.it">
+    edil2esse.it
+  </a>
+`
+
     await resend.emails.send({
       from: customDomainEmail,
       to: email,
       subject: 'Richiesta di preventivo ricevuta',
 
-      // TODO Replace email text with html or react (see resend documentation)
-      text: `
-        Ciao ${name},
-
-        Grazie per aver richiesto un preventivo. Abbiamo ricevuto la tua richiesta e ti contatteremo al più presto.
-
-        Cordiali saluti,
-        Il team di Edil 2 Esse
-      `,
+      html: userEmail,
     })
 
     console.log('Estimate request email sent successfully')
