041a41a523de7d008ee63b7eee6f9cfc206e13aa
emanuelefavero <emafaux89@gmail.com>
2024-11-05 17:29:38 +0100

    add dark mode style in estimate form

--- components/EstimateForm.tsx
+++ components/EstimateForm.tsx
@@ -1,7 +1,6 @@
 import { sendEstimateRequestEmail } from '@/app/actions'
 
 export default function Component() {
-  // TODO Style dark mode form
   // TODO add Button component as submit button
   // TODO Add form validation
   // TODO Add form submission success and error messages
@@ -19,14 +18,15 @@ export default function Component() {
           <div className='w-full md:w-1/2 px-2 mb-4'>
             <label
               htmlFor='name'
-              className='block text-sm font-medium text-stone-700 mb-2'
+              className='block text-sm font-medium text-stone-700 dark:text-stone-200 mb-2'
             >
-              Nome <span className='text-yellow-600'>*</span>
+              Nome{' '}
+              <span className='text-yellow-600 dark:text-yellow-500'>*</span>
             </label>
             <input
               type='text'
               name='name'
-              className='w-full px-2.5 py-2 font-medium text-stone-700 border border-stone-500/35 rounded-lg focus:outline-none focus:ring-0 focus:ring-offset-0 focus:border-yellow-600 placeholder:text-stone-400'
+              className='w-full px-2.5 py-2 font-medium dark:bg-stone-800 text-stone-700 dark:text-stone-200 border border-stone-500/35 rounded-lg focus:outline-none focus:ring-0 focus:ring-offset-0 focus:border-yellow-600 dark:focus:border-yellow-700 placeholder:text-stone-400 dark:placeholder:text-stone-500'
               placeholder='Il tuo Nome'
               required
             />
@@ -34,14 +34,14 @@ export default function Component() {
           <div className='w-full md:w-1/2 px-2 mb-4'>
             <label
               htmlFor='phone'
-              className='block text-sm font-medium text-stone-700 mb-2'
+              className='block text-sm font-medium text-stone-700 dark:text-stone-200 mb-2'
             >
               Telefono
             </label>
             <input
               type='tel'
               name='phone'
-              className='w-full px-2.5 py-2 font-medium text-stone-700 border border-stone-500/35 rounded-lg focus:outline-none focus:ring-0 focus:ring-offset-0 focus:border-yellow-600 placeholder:text-stone-400'
+              className='w-full px-2.5 py-2 font-medium dark:bg-stone-800 text-stone-700 dark:text-stone-200 border border-stone-500/35 rounded-lg focus:outline-none focus:ring-0 focus:ring-offset-0 focus:border-yellow-600 dark:focus:border-yellow-700 placeholder:text-stone-400 dark:placeholder:text-stone-500'
               placeholder='Il tuo numero di telefono'
               pattern='^(\+39\s?)?((3\d{2}\s?\d{6,7})|(0\d{1,4}\s?\d{5,10}))$'
             />
@@ -49,14 +49,15 @@ export default function Component() {
           <div className='w-full md:w-1/2 px-2'>
             <label
               htmlFor='email'
-              className='block text-sm font-medium text-stone-700 mb-2'
+              className='block text-sm font-medium text-stone-700 dark:text-stone-200 mb-2'
             >
-              Email <span className='text-yellow-600'>*</span>
+              Email{' '}
+              <span className='text-yellow-600 dark:text-yellow-500'>*</span>
             </label>
             <input
               type='email'
               name='email'
-              className='w-full px-2.5 py-2 font-medium text-stone-700 border border-stone-500/35 rounded-lg focus:outline-none focus:ring-0 focus:ring-offset-0 focus:border-yellow-600 placeholder:text-stone-400'
+              className='w-full px-2.5 py-2 font-medium dark:bg-stone-800 text-stone-700 dark:text-stone-200 border border-stone-500/35 rounded-lg focus:outline-none focus:ring-0 focus:ring-offset-0 focus:border-yellow-600 dark:focus:border-yellow-700 placeholder:text-stone-400 dark:placeholder:text-stone-500'
               placeholder='La tua email'
               required
             />
@@ -67,13 +68,14 @@ export default function Component() {
         <div>
           <label
             htmlFor='message'
-            className='block text-sm font-medium text-stone-700 mb-2'
+            className='block text-sm font-medium text-stone-700 dark:text-stone-200 mb-2'
           >
-            Messaggio <span className='text-yellow-600'>*</span>
+            Messaggio{' '}
+            <span className='text-yellow-600 dark:text-yellow-500'>*</span>
           </label>
           <textarea
             name='message'
-            className='w-full px-2.5 py-2 font-medium text-stone-700 border border-stone-500/35 rounded-lg focus:outline-none focus:ring-0 focus:ring-offset-0 focus:border-yellow-600 placeholder:text-stone-400'
+            className='w-full px-2.5 py-2 font-medium dark:bg-stone-800 text-stone-700 dark:text-stone-200 border border-stone-500/35 rounded-lg focus:outline-none focus:ring-0 focus:ring-offset-0 focus:border-yellow-600 dark:focus:border-yellow-700 placeholder:text-stone-400 dark:placeholder:text-stone-500'
             rows={4}
             placeholder='Il tuo messaggio'
             required
--- components/EstimateFormSection.tsx
+++ components/EstimateFormSection.tsx
@@ -1,12 +1,13 @@
 import EstimateForm from '@/components/EstimateForm'
 
 export default function Component() {
+  // TODO separate title into its own component (in this file)
   return (
-    <section className='bg-stone-100 text-stone-800 border-b-[2.5px] border-yellow-700/60 dark:border-yellow-700/80 px-8 2xl:px-16 py-36 flex justify-center items-center font-semibold'>
+    <section className='bg-stone-100 dark:bg-stone-900 px-8 2xl:px-16 py-36 flex justify-center items-center font-semibold'>
       <div className='w-full max-w-screen-2xl flex flex-col items-center justify-center'>
         <h2 className='text-sm 5xs:text-[2rem] leading-10 font-bold text-center'>
           Richiedi un preventivo{' '}
-          <span className='text-yellow-600'>gratuito</span>
+          <span className='text-yellow-600 dark:text-amber-500'>gratuito</span>
         </h2>
 
         <EstimateForm />
