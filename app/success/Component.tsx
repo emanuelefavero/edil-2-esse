70db3207b6641ecd7283b37a0f1db4a883c3846a
emanuelefavero <emafaux89@gmail.com>
2024-11-06 23:35:10 +0100

    refactor success page

--- app/success/Component.tsx
+++ app/success/Component.tsx
@@ -15,13 +15,18 @@ export default function Component() {
           EDIL 2 ESSE
         </div>
         <h1 className='text-stone-700 dark:text-white font-semibold text-lg 5xs:text-2xl 4xs:text-3xl 2xs:text-4xl 5xs:leading-[2.3rem] 4xs:leading-[3rem] 2xs:leading-[3.3rem] mb-4'>
-          Grazie{' '}
+          Grazie per averci contattato
           {name ? (
-            <span className='text-yellow-600 dark:text-yellow-500'>{name}</span>
+            <>
+              ,{' '}
+              <span className='text-yellow-600 dark:text-yellow-500'>
+                {name}
+              </span>
+            </>
           ) : (
             ''
-          )}{' '}
-          per averci contattato!
+          )}
+          !
         </h1>
         <p className='font-[500] hidden 4xs:block text-md text-pretty text-stone-700 dark:text-white'>
           Ti risponderemo al più presto. Nel frattempo ti abbiamo inviato un
