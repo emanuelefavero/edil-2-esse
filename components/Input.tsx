4f6c9694494caa425047998897e084ee44e5dffb
emanuelefavero <emafaux89@gmail.com>
2024-11-06 20:40:46 +0100

    fix syntax - hide red border if placeholder is shown

--- components/Input.tsx
+++ components/Input.tsx
@@ -49,7 +49,7 @@ export default function Component({
         id={name}
         name={name}
         className={clsx(
-          'w-full px-2.5 py-2 font-medium dark:bg-stone-800 text-stone-700 dark:text-stone-200 border border-stone-500/35 rounded-lg focus:outline-none focus:ring-0 focus:ring-offset-0 focus:border-yellow-600 dark:focus:border-yellow-700 placeholder:text-stone-400 dark:placeholder:text-stone-500 [&:not(:placeholder-shown)]:invalid:border-red-500 dark:[&:not(:placeholder-shown)]:invalid:border-red-700 peer',
+          'w-full px-2.5 py-2 font-medium dark:bg-stone-800 text-stone-700 dark:text-stone-200 border border-stone-500/35 rounded-lg focus:outline-none focus:ring-0 focus:ring-offset-0 focus:border-yellow-600 dark:focus:border-yellow-700 placeholder:text-stone-400 dark:placeholder:text-stone-500 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500 dark:invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-700 peer',
           className,
           disabled && 'opacity-50 cursor-not-allowed'
         )}
