d5a44fc257e8599eb9cbe82ec6db23033900f7c0
emanuelefavero <emafaux89@gmail.com>
2024-11-07 00:16:56 +0100

    fix build error - useSearchParams() should be wrapped in a suspense boundary

--- app/success/Component.tsx
+++ app/success/Component.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useSearchParams } from 'next/navigation'
-import Link from 'next/link'
 
 export default function Component() {
   const searchParams = useSearchParams()
@@ -9,47 +8,37 @@ export default function Component() {
   const email = searchParams.get('email')
 
   return (
-    <section className='w-full h-[calc(100vh)] min-h-[667px] max-h-[900px] pt-16 pb-4 px-10 dark:bg-[#151210]/40 flex flex-col justify-center items-center gap-10'>
-      <div className='text-center max-w-prose xl:scale-125 xl:mb-6'>
-        <div className='mb-2 text-lg text-stone-700 dark:text-white'>
-          EDIL 2 ESSE
-        </div>
-        <h1 className='text-stone-700 dark:text-white font-semibold text-lg 5xs:text-2xl 4xs:text-3xl 2xs:text-4xl 5xs:leading-[2.3rem] 4xs:leading-[3rem] 2xs:leading-[3.3rem] mb-4'>
-          Grazie per averci contattato
-          {name ? (
-            <>
-              ,{' '}
-              <span className='text-yellow-600 dark:text-yellow-500'>
-                {name}
-              </span>
-            </>
-          ) : (
-            ''
-          )}
-          !
-        </h1>
-        <p className='font-[500] hidden 4xs:block text-md text-pretty text-stone-700 dark:text-white'>
-          Ti risponderemo al più presto. Nel frattempo ti abbiamo inviato un
-          email di conferma{' '}
-          {email ? (
-            <>
-              all{"'"}indirizzo{' '}
-              <span className='text-yellow-600 dark:text-yellow-500'>
-                {email}
-              </span>
-            </>
-          ) : (
-            ''
-          )}
-          . A presto!
-        </p>
+    <>
+      <div className='mb-2 text-lg text-stone-700 dark:text-white'>
+        EDIL 2 ESSE
       </div>
-      <Link
-        href='/'
-        className='text-yellow-600 dark:text-yellow-500 text-xl font-semibold hover:underline'
-      >
-        {'←'} Torna alla home
-      </Link>
-    </section>
+      <h1 className='text-stone-700 dark:text-white font-semibold text-lg 5xs:text-2xl 4xs:text-3xl 2xs:text-4xl 5xs:leading-[2.3rem] 4xs:leading-[3rem] 2xs:leading-[3.3rem] mb-4'>
+        Grazie per averci contattato
+        {name ? (
+          <>
+            ,{' '}
+            <span className='text-yellow-600 dark:text-yellow-500'>{name}</span>
+          </>
+        ) : (
+          ''
+        )}
+        !
+      </h1>
+      <p className='font-[500] hidden 4xs:block text-md text-pretty text-stone-700 dark:text-white'>
+        Ti risponderemo al più presto. Nel frattempo ti abbiamo inviato un email
+        di conferma{' '}
+        {email ? (
+          <>
+            all{"'"}indirizzo{' '}
+            <span className='text-yellow-600 dark:text-yellow-500'>
+              {email}
+            </span>
+          </>
+        ) : (
+          ''
+        )}
+        . A presto!
+      </p>
+    </>
   )
 }
--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,5 +1,25 @@
 import Component from './Component'
+import { Suspense } from 'react'
+import Link from 'next/link'
 
 export default function Page() {
-  return <Component />
+  return <SuccessSection />
+}
+
+function SuccessSection() {
+  return (
+    <section className='w-full h-[calc(100vh)] min-h-[667px] max-h-[900px] pt-16 pb-4 px-10 dark:bg-[#151210]/40 flex flex-col justify-center items-center gap-10'>
+      <div className='text-center max-w-prose xl:scale-125 xl:mb-6'>
+        <Suspense fallback={<div>Caricamento...</div>}>
+          <Component />
+        </Suspense>
+      </div>
+      <Link
+        href='/'
+        className='text-yellow-600 dark:text-yellow-500 text-xl font-semibold hover:underline'
+      >
+        {'←'} Torna alla home
+      </Link>
+    </section>
+  )
 }
