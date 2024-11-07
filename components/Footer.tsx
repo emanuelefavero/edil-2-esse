f5c06ee47e25a28419f04ab59f03c91b63812a65
emanuelefavero <emafaux89@gmail.com>
2024-11-07 20:57:40 +0100

    fix footer responsivness

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,13 +2,13 @@ export default function Footer() {
   const year = new Date().getFullYear()
 
   return (
-    <footer className='w-full bg-yellow-600 text-white py-4 px-4 flex justify-center items-center gap-2 text-sm'>
+    <footer className='w-full bg-yellow-600 text-white py-4 px-4 flex flex-col xs:flex-row justify-center items-center gap-2 text-sm'>
       <p>&copy; {year} Edil 2 Esse</p>
 
-      <span className='hidden 5xs:inline-block text-white mx-1'>|</span>
+      <span className='hidden xs:inline-block text-white mx-1'>|</span>
 
       <a
-        className='hidden 5xs:inline-block font-medium hover:underline'
+        className='hidden 5xs:inline-block font-medium hover:underline text-center xs:text-left'
         href='https://emanuelefavero.com/'
         target='_blank'
         rel='noopener noreferrer'
