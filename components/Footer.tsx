59a3eec98f1939fa8e7828cbc0ffce4d2e8489a1
emanuelefavero <emafaux89@gmail.com>
2024-11-07 19:06:56 +0100

    change footer style

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,15 +2,13 @@ export default function Footer() {
   const year = new Date().getFullYear()
 
   return (
-    <footer className='w-full bg-yellow-600 text-white py-4 px-2 flex justify-center items-center gap-2 text-sm'>
+    <footer className='w-full bg-yellow-600 text-white py-4 px-4 flex justify-center items-center gap-2 text-sm'>
       <p>&copy; {year} Edil 2 Esse</p>
 
-      <span className='hidden 5xs:inline text-white dark:text-black mx-1'>
-        |
-      </span>
+      <span className='hidden 5xs:inline-block text-white mx-1'>|</span>
 
       <a
-        className='hidden 5xs:inline font-medium hover:underline'
+        className='hidden 5xs:inline-block font-medium hover:underline'
         href='https://emanuelefavero.com/'
         target='_blank'
         rel='noopener noreferrer'
