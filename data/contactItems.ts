80ed654d99f4b48601f933933aeab835b820ea1e
emanuelefavero <emafaux89@gmail.com>
2024-11-07 17:55:47 +0100

    add data/contactItems

---
+++ data/contactItems.ts
@@ -0,0 +1,12 @@
+export const contactItems = [
+  { id: 1, label: 'Indirizzo', value: 'Via Bengasi 97/B' },
+  { id: 2, label: 'Città', value: 'Livorno' },
+  { id: 3, label: 'CAP', value: '57124' },
+  { id: 4, label: 'Provincia', value: 'LI' },
+  { id: 5, label: 'Regione', value: 'Toscana' },
+  { id: 5, label: 'Paese', value: 'Italia' },
+  { id: 6, label: 'Telefono', value: '+39 347 3468027' },
+  { id: 7, label: 'Email', value: 'info@edil2esse.it' },
+  { id: 10, label: 'Orari', value: 'Lun-Ven: 9:00-18:00' },
+  { id: 8, label: 'P IVA', value: '01790840498' },
+]
