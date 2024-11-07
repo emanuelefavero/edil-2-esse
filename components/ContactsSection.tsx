071872b7948b0d584d9b4bbd371a5e0a686c5851
emanuelefavero <emafaux89@gmail.com>
2024-11-07 18:53:11 +0100

    change contact section text color

--- components/ContactsSection.tsx
+++ components/ContactsSection.tsx
@@ -4,7 +4,7 @@ export default function Component() {
   return (
     <section
       id='contatti'
-      className='bg-yellow-600 text-white dark:text-black border-t-[2.5px] border-yellow-700/60 dark:border-yellow-700/80 px-8 2xl:px-16 py-36 flex justify-center items-center font-semibold'
+      className='bg-yellow-600 text-white border-t-[2.5px] border-yellow-700/60 dark:border-yellow-700/80 px-8 2xl:px-16 py-36 flex justify-center items-center font-semibold'
     >
       <div className='w-full max-w-screen-2xl flex flex-wrap items-center justify-center'>
         <div>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   const year = new Date().getFullYear()
 
   return (
-    <footer className='w-full bg-yellow-600 text-white dark:text-black py-4 px-2 flex justify-center items-center gap-2 text-sm'>
+    <footer className='w-full bg-yellow-600 text-white py-4 px-2 flex justify-center items-center gap-2 text-sm'>
       <p>&copy; {year} Edil 2 Esse</p>
 
       <span className='hidden 5xs:inline text-white dark:text-black mx-1'>
