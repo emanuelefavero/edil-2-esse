72a5b22a5ca8bd3133ee05ddce5638a40bdd7a35
emanuelefavero <emafaux89@gmail.com>
2024-11-07 20:24:14 +0100

    change map src

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,14 +1,14 @@
 export default function Component() {
   return (
     <iframe
-      src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1445.8803722333867!2d10.335284!3d43.549029!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12d5e9590478ed51%3A0x6b1a3d4dca66e6c6!2sVia%20Bengasi%2C%2097%2FB%2C%2057124%20Livorno%20LI!5e0!3m2!1sit!2sit!4v1731005160111!5m2!1sit!2sit'
+      src='https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d1612.8224860312173!2d10.334680526226451!3d43.54935058817961!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDPCsDMyJzU2LjIiTiAxMMKwMjAnMDUuNyJF!5e0!3m2!1sen!2sit!4v1731007391518!5m2!1sen!2sit'
       // width='400'
       // height='200'
       style={{ border: 0 }}
       allowFullScreen={true}
       loading='lazy'
       referrerPolicy='no-referrer-when-downgrade'
-      className='rounded-md shadow-[0_0_0_3px] shadow-amber-600/80 dark:shadow-yellow-700/80 hidden md:block w-80'
+      className='rounded-md shadow-[0_0_0_2.5px] shadow-yellow-700/80 hidden md:block w-80'
     ></iframe>
   )
 }
