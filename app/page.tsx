b749b0948b0053d7bb010b8fb1544024f5020f3b
emanuelefavero <emafaux89@gmail.com>
2024-11-07 17:41:08 +0100

    add contacts section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Banner from '@/components/Banner'
 import HeroSection from '@/components/HeroSection'
 import ServicesSection from '@/components/ServicesSection'
 import EstimateFormSection from '@/components/EstimateFormSection'
+import ContactsSection from '@/components/ContactsSection'
 
 export default function Home() {
   // TODO: Add contacts section with id='contatti'
@@ -11,6 +12,7 @@ export default function Home() {
       <Banner />
       <ServicesSection />
       <EstimateFormSection />
+      <ContactsSection />
     </>
   )
 }
---
+++ components/ContactsSection.tsx
@@ -0,0 +1,14 @@
+export default function Component() {
+  return (
+    <section
+      id='contatti'
+      className='bg-yellow-600 border-t-[2.5px] border-yellow-700/60 dark:border-yellow-700/80 px-8 2xl:px-16 py-36 flex justify-center items-center font-semibold'
+    >
+      <div className='w-full max-w-screen-2xl flex flex-wrap items-center justify-between'>
+        <div>
+          <h2 className='text-3xl'>Edil 2 Esse</h2>
+        </div>
+      </div>
+    </section>
+  )
+}
