de0395dd35f3a57fdbc693152adcb70870d71fa6
emanuelefavero <emafaux89@gmail.com>
2024-11-07 01:50:07 +0100

    udpate menu items

--- data/menuItems.ts
+++ data/menuItems.ts
@@ -1,7 +1,6 @@
-// TODO Update menu items to reflect the actual menu items
 export const menuItems = [
   { id: 'home', label: 'Home' },
   { id: 'servizi', label: 'Servizi' },
-  { id: 'chiSiamo', label: 'Chi Siamo' },
+  { id: 'preventivo', label: 'Preventivo' },
   { id: 'contatti', label: 'Contatti' },
 ]
