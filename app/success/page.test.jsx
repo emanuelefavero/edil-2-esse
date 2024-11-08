d8b96c2fd4ba1e1a5783efe449f697e4316f54a3
emanuelefavero <emafaux89@gmail.com>
2024-11-09 00:42:04 +0100

    move jest mock functions to jest.setup.ts

--- app/success/page.test.jsx
+++ app/success/page.test.jsx
@@ -2,33 +2,6 @@ import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import Page from './page'
 
-// Mock Resend
-jest.mock('resend', () => {
-  return {
-    Resend: jest.fn().mockImplementation(() => ({
-      emails: { send: jest.fn() },
-    })),
-  }
-})
-
-// Mock next/navigation
-jest.mock('next/navigation', () => {
-  return {
-    __esModule: true,
-    usePathname: () => ({
-      pathname: '',
-    }),
-    useRouter: () => ({
-      push: jest.fn(),
-      replace: jest.fn(),
-      prefetch: jest.fn(),
-    }),
-    useSearchParams: () => ({
-      get: () => {},
-    }),
-  }
-})
-
 describe('Page', () => {
   it('renders the h1 heading', () => {
     render(<Page />)
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,24 @@
 import '@testing-library/jest-dom'
+
+// Mock Resend
+jest.mock('resend', () => ({
+  Resend: jest.fn().mockImplementation(() => ({
+    emails: { send: jest.fn() },
+  })),
+}))
+
+// Mock next/navigation
+jest.mock('next/navigation', () => ({
+  __esModule: true,
+  usePathname: () => ({
+    pathname: '',
+  }),
+  useRouter: () => ({
+    push: jest.fn(),
+    replace: jest.fn(),
+    prefetch: jest.fn(),
+  }),
+  useSearchParams: () => ({
+    get: () => {},
+  }),
+}))
