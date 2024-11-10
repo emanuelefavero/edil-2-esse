02c1420f6ed9a2c280ae13ea9a65259a1f847629
emanuelefavero <emafaux89@gmail.com>
2024-11-10 02:38:41 +0100

    remove fill in svg and add custom colors comments

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,8 @@
 import type { Config } from 'tailwindcss'
 import plugin from 'tailwindcss/plugin'
 
-// TODO add tailwind color global variables (e.g. text-primary, bg-primary)
+// * Uncomment the following line to use the default Tailwind CSS colors
+// import colors from 'tailwindcss/colors'
 
 const config: Config = {
   content: [
@@ -26,10 +27,68 @@ const config: Config = {
     },
 
     extend: {
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-      },
+      // * Add custom colors or change the default colors here
+      // colors: {
+      //   // primaryBackground: 'var(--background)',
+      //   // primaryForeground: 'var(--foreground)',
+      //   primary: colors.yellow, // e.g. text-primary-600
+      //   white: colors.white,
+      //   black: colors.black,
+      //   background: colors.stone,
+      //   foreground: colors.stone,
+      //   stone: {
+      //     50: '#f9f9f9',
+      //     100: '#f5f5f4',
+      //     200: '#e7e5e4',
+      //     300: '#d6d3d1',
+      //     400: '#a8a29e',
+      //     500: '#78716c',
+      //     600: '#57534e',
+      //     700: '#44403c',
+      //     800: '#292524',
+      //     900: '#1c1917',
+      //     950: '#0c0a09',
+      //   },
+      //   amber: {
+      //     50: '#fffbeb',
+      //     100: '#fef3c7',
+      //     200: '#fde68a',
+      //     300: '#fcd34d',
+      //     400: '#fbbf24',
+      //     500: '#f59e0b',
+      //     600: '#d97706',
+      //     700: '#b45309',
+      //     800: '#92400e',
+      //     900: '#78350f',
+      //     950: '#451a03',
+      //   },
+      //   yellow: {
+      //     50: '#fefce8',
+      //     100: '#fef9c3',
+      //     200: '#fef08a',
+      //     300: '#fde047',
+      //     400: '#facc15',
+      //     500: '#eab308',
+      //     600: '#ca8a04',
+      //     700: '#a16207',
+      //     800: '#854d0e',
+      //     900: '#713f12',
+      //     950: '#422006',
+      //   },
+      //   red: {
+      //     50: '#fef2f2',
+      //     100: '#fee2e2',
+      //     200: '#fecaca',
+      //     300: '#fca5a5',
+      //     400: '#f87171',
+      //     500: '#ef4444',
+      //     600: '#dc2626',
+      //     700: '#b91c1c',
+      //     800: '#991b1b',
+      //     900: '#7f1d1d',
+      //     950: '#3f1212',
+      //   },
+      // },
 
       keyframes: {
         slideDownAndFade: {
