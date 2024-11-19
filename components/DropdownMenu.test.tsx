e7139932e18ca5ae63077fb18ff4df9af806623a
emanuelefavero <emafaux89@gmail.com>
2024-11-19 23:05:32 +0100

    add comments in DropdownMenu test

--- components/DropdownMenu.test.tsx
+++ components/DropdownMenu.test.tsx
@@ -2,6 +2,8 @@ import { render, screen } from '@testing-library/react'
 import Component from './DropdownMenu'
 import { useDarkMode } from '@/hooks/useDarkMode'
 
+// NOTE: Since the dropdown menu is a Radix UI component that renders a portal, testing this component is more complex. We already tested the menu items in the Menu component, so we decided to only test the trigger button in this component.
+
 // Mock useDarkMode hook
 jest.mock('@/hooks/useDarkMode', () => ({
   useDarkMode: jest.fn(),
