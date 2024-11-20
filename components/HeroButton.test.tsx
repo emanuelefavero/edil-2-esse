e19ba28e0585a05e00cb844894d3301977a58dac
emanuelefavero <emafaux89@gmail.com>
2024-11-20 20:59:09 +0100

    add HeroButton test - render link with correct href

--- components/HeroButton.test.tsx
+++ components/HeroButton.test.tsx
@@ -2,10 +2,16 @@ import { render, screen } from '@testing-library/react'
 // import userEvent from '@testing-library/user-event'
 import Component from './HeroButton'
 
-describe('Component', () => {
+describe('HeroButton', () => {
   it('renders the link with the correct text', () => {
     render(<Component />)
     const link = screen.getByRole('link', { name: /i nostri servizi/i })
     expect(link).toHaveTextContent('I NOSTRI SERVIZI')
+  })
+
+  it('renders the link with the correct href', () => {
+    render(<Component />)
+    const link = screen.getByRole('link', { name: /i nostri servizi/i })
+    expect(link).toHaveAttribute('href', '/#servizi')
   })
 })
