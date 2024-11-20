87b735d3e7a7d0790f934bf9de44e3973d0f13a2
emanuelefavero <emafaux89@gmail.com>
2024-11-20 20:57:31 +0100

    update HeroButton to be a link instead of a button

--- components/HeroButton.test.tsx
+++ components/HeroButton.test.tsx
@@ -1,34 +1,11 @@
 import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import { useRouter } from 'next/navigation'
+// import userEvent from '@testing-library/user-event'
 import Component from './HeroButton'
 
-// Mock the `useRouter` hook from Next.js
-jest.mock('next/navigation', () => ({
-  useRouter: jest.fn(),
-}))
-
-// Mock the `push` method from the `useRouter` hook
-const push = jest.fn()
-;(useRouter as jest.Mock).mockReturnValue({ push })
-
 describe('Component', () => {
-  it('renders the button with the correct text', () => {
+  it('renders the link with the correct text', () => {
     render(<Component />)
-
-    // Check for both the hidden and visible text in the button
-    expect(screen.getByText('SERVIZI')).toBeInTheDocument()
-    expect(screen.getByText('I NOSTRI')).toBeInTheDocument()
-  })
-
-  it('redirects to "/#servizi" when clicked', async () => {
-    render(<Component />)
-
-    // Simulate a user click on the button
-    const button = screen.getByRole('button', { name: /i nostri servizi/i })
-    await userEvent.click(button)
-
-    // Ensure the `push` method was called with the correct URL
-    expect(push).toHaveBeenCalledWith('/#servizi')
+    const link = screen.getByRole('link', { name: /i nostri servizi/i })
+    expect(link).toHaveTextContent('I NOSTRI SERVIZI')
   })
 })
--- components/HeroButton.tsx
+++ components/HeroButton.tsx
@@ -1,17 +1,12 @@
-'use client'
-
-import Button from '@/components/Button'
-import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 
 export default function Component() {
-  const router = useRouter()
-
   return (
-    <Button
-      className='5xs:text-md text-sm 3xs:text-lg'
-      onClick={() => router.push('/#servizi')}
+    <Link
+      href={`/#servizi`}
+      className='5xs:text-md relative select-none rounded-lg border-[1px] border-amber-400 bg-yellow-700 px-5 py-1.5 text-sm font-[600] tracking-wide text-white shadow-[0.5px_0.5px_0.5px_0.8px] shadow-yellow-800/80 outline-none transition-all duration-200 text-shadow-yellow hover:bg-yellow-600 focus:bg-yellow-600 active:translate-y-0.5 active:scale-[0.99] active:transform active:shadow-none disabled:cursor-not-allowed disabled:opacity-50 3xs:text-lg'
     >
       <span className='hidden 3xs:inline'>I NOSTRI</span> SERVIZI
-    </Button>
+    </Link>
   )
 }
